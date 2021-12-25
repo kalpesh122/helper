b570249121aefa12c3853f4630cd97baeb81d129
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-25 23:04:00 +0700

    Setup

--- pages/login.js
+++ pages/login.js
@@ -6,7 +6,13 @@ export default () => (
             <title>Login with Google</title>
             <link rel="stylesheet" href="/stylesheets/login.css" />
         </Head>
-        <button>Login with Google</button>
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+        <div className="google-btn">
+            <div className="google-icon-wrapper">
+                <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" />
+            </div>
+            <p className="btn-text"><b>Sign in with google</b></p>
+        </div>
     </>
 );
 
